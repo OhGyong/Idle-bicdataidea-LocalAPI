e9e05b6eddb91ba076f9a51853bef81a8cb16bf8
OhGyong <oky7143@naver.com>
2021-04-07 19:02:46 +0900

    공고정보 내용 가져와서 db 넣는거 해야함

--- API 작업/API 기능작성/routes/admins.js
+++ API 작업/API 기능작성/routes/admins.js
@@ -871,42 +871,59 @@ router.get('/idle/board/announcement', (req, res) => {
 
     getConnection(async (conn) => {
         try {
+            console.log(1) // 비동기 처리 확인
 
             // 크롤링 시작
-            const getHtml = async () => {
-                try {
-                    console.log(1)
-                    return await axios.get("https://library.hallym.ac.kr/bbs/list/4"); // 학교 도서관 페이지
-                } catch (error) {
-                    console.error(error);
-                }
-            };
-            getHtml().then(html => {
-                const $ = cheerio.load(html.data);
-                const $bodyList = $("#divList > table > tbody > tr"); // 데이터를 뽑아올 위치
-                $bodyList.each(  function (i, elem) {
-                    console.log(2)
-                    ulList[i] = {
-                        num: $(elem).find('td.num').text().trim(),
-                        title: $(elem).find('td.title').text().trim(),
-                        date: $(elem).find('td.insert_date').text(),
-                        url: $(elem).find('td.title a').attr("href")
+            await new Promise((res, rej) => {
+                const getHtml = () => {
+                    try {
+                        console.log(2)
+                        return axios.get("https://library.hallym.ac.kr/bbs/list/4"); // 학교 도서관 페이지
+                    } catch (error) {
+                        console.error(error);
                     }
-                });
-                const data = ulList.filter(n => n.num);
-                return data;
-            }).catch((err) => {
-                rej(err)
-            }).then(res => console.log(res));
+                };
+                getHtml().then(html => {
+                    const $ = cheerio.load(html.data);
+                    const $bodyList = $("#divList > table > tbody > tr"); // 데이터를 뽑아올 위치
+                    $bodyList.each(function (i, elem) {
+                        console.log(3)
+                        ulList[i] = {
+                            num: $(elem).find('td.num').text().trim(),
+                            title: $(elem).find('td.title').text().trim(),
+                            date: $(elem).find('td.insert_date').text(),
+                            url: $(elem).find('td.title a').attr("href")
+                        }
+                    });
+                    console.log(4)
+                    const data = ulList.filter(n => n.num);
+                    res(data);
+                }).catch((err) => {
+                    rej(err)
+                })
+            })
 
+            // 크롤링한 데이터 수정 (공지 게시물은 제거), splice 하면 return 되서 for문 종료되기 때문에 null 값이 몇 개인지만 찾는다.
+            var catch_null = 0;
+            for(k in ulList){
+                if(ulList[k].num==''){
+                    console.log("몇번? : "+ k);
+                    catch_null=k;
+                }
+            }
+            catch_null++; // index 값 때문에 1 추가
+            ulList.splice(0,catch_null);
 
             // db 입력 부분
             await new Promise((res, rej) => {
-                console.log(3)
+                console.log(5)
+                console.log(ulList)
+                //var anno_list_sql='INSERT INTO anno (anno_id, anno_title, anno_contents, anno_date, anno_link) VALUES (?,?,?,?,?)'
+                //var anno_list_params;
                 res();
             })
 
-            console.log(4)
+            console.log(6)
         } catch (err) {
 
         }
